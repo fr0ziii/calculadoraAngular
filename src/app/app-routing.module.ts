63ff2303deb236f2e4868ae98bf86029908f8ae9
fr0ziii <fr0ziisc2@gmail.com>
2022-03-25 16:48:43 +0000

    Final version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { ReadmeComponent } from './readme/readme.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'calculadora', component: CalculatorComponent },
+  { path: 'readme', component: ReadmeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
